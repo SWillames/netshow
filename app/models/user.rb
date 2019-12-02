219637ddc4c6a785d7945a74465c5551e55cf192
SWillames <sergiowoc@gmail.com>
2019-12-02 18:20:25 -0300

    introduces user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :first_name, :email, :last_name, :user_name, :password
+  validates_uniqueness_of :email, :user_name
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:user_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it 'expected validate uniqueness of email' do
+      user = create(:user)
+      user1 = build(:user, email: user.email)
+      expect validate_uniqueness_of(:email).ignoring_case_sensitivity
+      expect(user1).not_to be_valid
+      expect(user1.errors[:email]).to be_present
+    end
+   it {is_expected.to validate_uniqueness_of(:user_name).ignoring_case_sensitivity}
+  end
+
+
+  
 end
