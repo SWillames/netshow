65bcf281dc573bf3a2fe466cacff42bfd034fabb
SWillames <sergiowoc@gmail.com>
2019-12-02 18:12:55 -0300

    Introduces user factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
+    user_name {Faker::Internet.username(specifier: "#{first_name} #{last_name}", separators: %w(. _ -))}
+    email {Faker::Internet.unique.email}    
+    password {Faker::Internet.password}    
   end
 end
