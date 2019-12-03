e94c1f89f803d33b9931059477b613b0009496e1
SWillames <sergiowoc@gmail.com>
2019-12-03 10:00:02 -0300

    defining the association between user and video.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :videos, dependent: :destroy
+
   validates_presence_of :first_name, :email, :last_name, :user_name, :password
   validates_uniqueness_of :email, :user_name
 
