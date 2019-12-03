75372cac2ed2a7ec6382447a504bd77396974c15
SWillames <sergiowoc@gmail.com>
2019-12-03 10:06:02 -0300

    db video table and schema migration defined.

--- db/migrate/20191203115054_create_videos.rb
+++ db/migrate/20191203115054_create_videos.rb
@@ -3,7 +3,7 @@ class CreateVideos < ActiveRecord::Migration[5.2]
     create_table :videos do |t|
       t.string :title
       t.string :url
-      t.integer :cont_view
+      t.integer :count_view
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_120533) do
+ActiveRecord::Schema.define(version: 2019_12_03_115054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,15 @@ ActiveRecord::Schema.define(version: 2019_12_02_120533) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.integer "count_view"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
+  end
+
+  add_foreign_key "videos", "users"
 end
