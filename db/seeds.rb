d52ac0f4d4f06977ff5b5c63eeb21e76cec2bbca
SWillames <sergiowoc@gmail.com>
2019-12-04 13:46:27 -0300

    populating the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+user_count = 1
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    user_name: Faker::Internet.username
+
+  )
+  user_count += 1
+
+  user.save!
+end
+
+url_array = ['https://content.jwplatform.com/manifests/yp34SRmf.m3u8', 'https://cdn.jwplayer.com/manifests/RltV8MtT.m3u8']
+
+video_count = 1
+20.times do
+  user_id = User.all.sample
+  video = Video.new(
+    title: Faker::DcComics.title,
+    url: url_array.sample,
+    count_view: 0,
+    user: user_id
+  )
+  video_count += 1
+
+  video.save!
+end
