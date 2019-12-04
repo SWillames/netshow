d9df8977af3a252449c860a82be891fce141f094
SWillames <sergiowoc@gmail.com>
2019-12-04 13:51:14 -0300

    video factory.

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :video do
     title { Faker::Movie.quote }
     url { 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8' }
-    cont_view { 1 }
+    count_view { 1 }
     user { create(:user) }
   end
 end
