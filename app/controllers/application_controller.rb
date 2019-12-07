343c7367569b5cd3624d33b1a0dd6edf6dcf8de5
SWillames <sergiowoc@gmail.com>
2019-12-07 03:24:06 -0300

    adding new rota configure  permitted parameters.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:first_name, :last_name, :user_name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name])
+
+  end
 end
